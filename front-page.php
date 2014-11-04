11cd75ebdcde5312a51f471f7da06c8c712b7b98
franszo <josh@kishandchips.com>
2014-11-04 15:12:49 +0000

    Front Page - Quick Links & User comfortable Link select Functionality
    Signed-off-by: franszo <josh@kishandchips.com>

--- front-page.php
+++ front-page.php
@@ -3,8 +3,9 @@
 	<div class="inner">
 
 	<!-- if is FRONT-PAGE -->
-
 	<?php if (is_front_page()): ?>
+
+		<!-- CAROUSEL -->
 		<?php if( have_rows('front_page_slider') ): ?>
 			<div class="owl-carousel">
 		  	<?php while ( have_rows('front_page_slider') ) : the_row(); ?>  
@@ -31,7 +32,47 @@
 		    <?php endwhile; ?>
 		    </div>
 		<?php endif; ?>
-	<?php endif; ?>
+
+		<!-- QUICK Links -->
+		<?php if( have_rows('quick_link_boxes') ): ?>
+			<div id="quick-links" class="clearfix">
+		  		<?php while ( have_rows('quick_link_boxes') ) : the_row(); ?>  
+		  	
+					<div class="span third">
+						<?php $type = get_sub_field('link_type'); ?>
+						<?php if($type == 'category'): ?>
+							<?php 
+								$category_id = get_sub_field('box_link_prod_cat');
+							 ?>
+							 <a href="<?php echo get_term_link($category_id, 'product_cat'); ?>">
+							
+						<?php else: ?>
+	  						<?php 
+							$posts = get_sub_field('box_link_page');
+							if( $posts ): ?>
+								<?php foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
+								    	<a class="link" href="<?php echo get_permalink( $p ); ?>">
+								<?php endforeach; ?>
+							<?php endif; ?>
+						<?php endif; ?>
+
+		 						<?php 
+									$attachment_id = get_sub_field('box_image');
+									$imgsrc = wp_get_attachment_image_src($attachment_id,array(380, 220, 'bfi_thumb' => true) );
+									$image = bfi_thumb( $imgsrc[0] );
+								?>
+								<img src="<?php echo $image;  ?>" alt="">
+								<h1><?php the_sub_field('box_title'); ?></h1>					
+							</a>
+					</div>
+		    <?php endwhile; ?>
+		    	</div>
+		    </div>
+		<?php endif; ?><!-- end IF quick_link_boxes -->
+	<?php endif; ?>	<!-- end IF Frontpage -->
+	<?php wp_reset_postdata(); ?>
+
+
 
 	<?php while ( have_posts() ) : the_post(); ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
