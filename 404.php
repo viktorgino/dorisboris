a923a8d9e621c32f48cfa6fde96e0d98fd027267
franszo <josh@kishandchips.com>
2014-11-19 15:36:20 +0000

    fixes
    Signed-off-by: franszo <josh@kishandchips.com>

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
 					<div class="span column seven break">
 						<div class="error">
 							<h1>Oops!</h1>                     
-							<h2>It’s looking like you may have taken a wrong turn.
+							<h2>It’s look like you may have taken a wrong turn.
 							Don’t worry... it happens to the best of us.</h2>
 							<p>Theres’s a little help on the top that might help you get back on track.</p>
 							<span>404</span>
